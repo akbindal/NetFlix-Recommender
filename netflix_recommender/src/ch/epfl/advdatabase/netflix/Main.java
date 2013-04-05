c2ef67f8b18cbbfe470ac10bcc4eab135e8ac375
akbindal <ashish.bindal@epfl.ch>
2013-04-05 03:30:26 +0200

    main clean

--- netflix_recommender/src/ch/epfl/advdatabase/netflix/Main.java
+++ netflix_recommender/src/ch/epfl/advdatabase/netflix/Main.java
@@ -3,12 +3,6 @@ package ch.epfl.advdatabase.netflix;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.jobcontrol.Job;
 import org.apache.hadoop.mapred.jobcontrol.JobControl;
@@ -16,11 +10,7 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import ch.epfl.advdatabase.netflix.preprocessing.ColNormMatrix;
-import ch.epfl.advdatabase.netflix.preprocessing.ColNormMatrix.TransposeMapper;
-import ch.epfl.advdatabase.netflix.preprocessing.ColNormMatrix.TransposeReducer;
 import ch.epfl.advdatabase.netflix.preprocessing.RowNormMatrix;
-import ch.epfl.advdatabase.netflix.preprocessing.RowNormMatrix.UserRowMapper;
-import ch.epfl.advdatabase.netflix.preprocessing.RowNormMatrix.UserRowReducer;
 import ch.epfl.advdatabase.netflix.setting.IOInfo;
 
 public class Main extends Configured implements Tool{
@@ -31,37 +21,7 @@ public class Main extends Configured implements Tool{
 			System.exit(-1);
 		}
 		int res = ToolRunner.run(new Configuration(), new Main(), args);
-		 
 		System.exit(res);
-//		JobConf confRowMatrix = RowNormMatrix.getConfRNormMatrix(args[0], IOInfo.CACHE_ROW_MATRIX);
-//		//JobConf confMovieStat = JobConfig.getMovieStat(args[0]);
-//		JobConf confColMatrix = ColNormMatrix.getConfTransMatrix(IOInfo.CACHE_ROW_MATRIX, IOInfo.CACHE_COL_MATRIX);
-//		
-//		//Job jobMovieStat = new Job(confMovieStat);
-//		Job jobRowNormMatrix = new Job(confRowMatrix);
-//		Job jobColNormMatrix = new Job(confColMatrix);
-//		jobColNormMatrix.addDependingJob(jobRowNormMatrix);
-//		JobControl jc = new JobControl("create norm matrix");
-//		jc.addJob(jobRowNormMatrix);
-//		jc.addJob(jobColNormMatrix);
-		
-		//jc.addJob(jobMovieStat);	
-		//matrix normalization
-		//calculate 
-//		JobConf confMovieStat = JobConfig.getMovieStat(args[0]);
-//		JobConf confUserStat = JobConfig.getUserStat(args[0]);
-//		//JobConf confNormMatrix = JobConfig.getNormMatrix();
-//		Job jobMovieStat = new Job(confMovieStat);
-//		Job jobUserStat = new Job(confUserStat);
-//		//Job jobNormMatrix = new Job(confNormMatrix);
-//		//jobNormMatrix.addDependingJob(jobMovieStat);
-//		//jobNormMatrix.addDependingJob(jobUserStat);
-//		JobControl jc = new JobControl("create normmatrix");
-//		jc.addJob(jobMovieStat);					
-//		jc.addJob(jobUserStat);
-		//jc.run();
-		
-		System.out.println("next after conf");
 	}
 
 	@Override
